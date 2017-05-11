efdb49fd6b0edbd4d0c7935f3d532704eacacc67
zsnydr <zackwsnyder@gmail.com>
2017-05-10 18:32:54 -0700

    [add] add styles and animation

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,18 +1,106 @@
 $(document).ready(function() {
-  console.log('Document is ready');
-
   $('.btn').on('click', function() {
     const lookup = {
       'first-btn': '#second',
       'second-btn': '#third',
-      'third-btn': '#fourth',
-      'fourth-btn': '#first'
+      'third-btn': '#first',
     };
     const classList = this.classList;
-    var next = $(lookup[classList[1]]); // jQyery doesn't like const or let
-    const position = next.position();
-    $('body').animate({
+    const target = lookup[classList[1]];
+    var $next = $(target); // jQyery doesn't like const or let
+    const position = $next.position();
+    $('body, html').animate({
       scrollTop: position.top
-    }, 1000);
+    }, 1000, function() {
+      switch (target) {
+        case '#second':
+          fadeInSecond();
+          break;
+        case '#third':
+          fadeInThird();
+          break;
+      }  
+    });
   });
+  fadeInFirst();
 });
+
+function fadeInFirst() {
+  setTimeout(function() {
+    var $firstContent = $('.first-content');
+    $firstContent.css('border-color', '#FFF');
+  }, 500);
+
+  setTimeout(function() {
+    var $firstContentSpan = $('.first-content-span');
+    var $horizontalLine = $('.horizontal-line');
+    $firstContentSpan.css('color', '#FFF');
+    $horizontalLine.css('border-color', '#FFF');
+  }, 2000);
+
+  setTimeout(function() {
+    var $firstBtn = $('.first-btn');
+    $firstBtn.fadeIn(500);
+  }, 4000);
+}
+
+function fadeInSecond() {
+  var $secondContent = $('.second-content');
+  $secondContent.css('border-color', '#000');
+  setTimeout(function() {
+    var $secondContentSpan = $('.second-content-span');
+    $secondContentSpan.fadeIn(1000, function() {
+      setTimeout(function() {
+        $secondContentSpan.fadeOut(1000, function() {
+          $secondContentSpan.html('for all the sacrifices you make to keeep our family strong');
+          $secondContentSpan.fadeIn(1000, function() {
+            setTimeout(function() {
+              $secondContentSpan.fadeOut(1000, function() {
+                $secondContentSpan.html('and for always believing in my dreams.');
+                $secondContentSpan.fadeIn(1000, function() {
+                  setTimeout(function() {
+                    $secondContentSpan.fadeOut(1000, function() {
+                      $secondContentSpan.html('You are the one person I can always count on');
+                      $secondContentSpan.fadeIn(1000, function() {
+                        var $secondBtn = $('.second-btn');
+                        $secondBtn.fadeIn(500);
+                      });
+                    });
+                  }, 3000);
+                });
+              });
+            }, 3000);
+          });
+        });
+      }, 3000);
+    });
+  }, 1000);
+}
+
+function fadeInThird() {
+  setTimeout(function() {
+    var $thirdBtn = $('.third-btn');
+    $thirdBtn.css('border-top', '25px solid #DD8183');
+  }, 2000);
+}
+
+function fadeInThird() {
+  setTimeout(function() {
+    var $thirdContent = $('.third-content');
+    $thirdContent.css('border-color', '#FFF');
+  }, 500);
+
+  setTimeout(function() {
+    var $thirdContentSpan = $('.third-content-span');
+    $thirdContentSpan.css('color', '#FFF');
+    setTimeout(function() {
+      var $thirdContentSpanTwo = $('.third-content-span-two');
+      $thirdContentSpanTwo.css('color', '#FFF');
+    }, 1500);
+  }, 1500);
+
+  setTimeout(function() {
+    var $thirdBtn = $('.third-btn');
+    $thirdBtn.fadeIn(500);
+  }, 4000);
+}
